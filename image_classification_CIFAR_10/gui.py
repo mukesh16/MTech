3c7a9d9fcd5f3b56a1fc7b4bca096d464ad62d7a
Mukesh Kumar Sahu <sahumukeshkumar16@gmail.com>
2024-02-12 23:26:54 +0530

    Update gui.py

--- image_classification_CIFAR_10/gui.py
+++ image_classification_CIFAR_10/gui.py
@@ -4,12 +4,12 @@ from tkinter import *
 from PIL import ImageTk, Image
 import numpy as np
 
-#load the trained model to classify the images
+# load the trained model to classify the images
 
 from keras.models import load_model
 model = load_model('model1_cifar_10epoch.h5')
 
-#dictionary to label all the CIFAR-10 dataset classes.
+# dictionary to label all the CIFAR-10 dataset classes.
 
 classes = { 
     0:'aeroplane',
@@ -23,19 +23,19 @@ classes = {
     8:'ship',
     9:'truck' 
 }
-#initialise GUI
+# initialise GUI
 
-top=tk.Tk()
+top = tk.Tk()
 top.geometry('800x600')
 top.title('Image Classification CIFAR10')
-top.configure(background='#CDCDCD')
-label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+top.configure(background = '#CDCDCD')
+label = Label(top,background = '#CDCDCD', font=('arial', 15, 'bold'))
 sign_image = Label(top)
 
 def classify(file_path):
     global label_packed
     image = Image.open(file_path)
-    image = image.resize((32,32))
+    image = image.resize((32, 32))
     image = np.expand_dims(image, axis=0)
     image = np.array(image)
     # pred = model.predict_classes([image])[0]
@@ -44,40 +44,42 @@ def classify(file_path):
     pred = model.predict([image])[0]
     sign = classes[np.argmax(pred)]
     print(sign)
-    label.configure(foreground='#011638', text=sign) 
+    label.configure(foreground = '#011638', text = sign) 
 
 def show_classify_button(file_path):
-    classify_b=Button(top,text="Classify Image",
-        command=lambda: classify(file_path),padx=10,pady=5)
-    classify_b.configure(background='#364156', foreground='white',
-        font=('arial',10,'bold'))
-    classify_b.place(relx=0.79,rely=0.46)
+    classify_b = Button(top, text = "Classify Image",
+                        command = lambda: classify(file_path),
+                        padx = 10, pady = 5)
+    classify_b.configure(background = '#364156', foreground = 'white',
+                         font = ('arial', 10, 'bold'))
+    classify_b.place(relx = 0.79, rely = 0.46)
 
 def upload_image():
     try:
-        file_path=filedialog.askopenfilename()
-        uploaded=Image.open(file_path)
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
         uploaded.thumbnail(((top.winfo_width()/2.25),
-            (top.winfo_height()/2.25)))
+                            (top.winfo_height()/2.25)))
         im=ImageTk.PhotoImage(uploaded)
-        sign_image.configure(image=im)
-        sign_image.image=im
-        label.configure(text='')
+        sign_image.configure(image = im)
+        sign_image.image = im
+        label.configure(text = '')
         show_classify_button(file_path)
     except:
         pass
 
-upload=Button(top,text="Upload an image",command=upload_image,
-  padx=10,pady=5)
+upload = Button(top, text = "Upload an image",
+                command = upload_image, padx = 10, pady = 5)
 
-upload.configure(background='#364156', foreground='white',
-    font=('arial',10,'bold'))
+upload.configure(background = '#364156', foreground = 'white',
+                 font = ('arial', 10, 'bold'))
 
-upload.pack(side=BOTTOM,pady=50)
-sign_image.pack(side=BOTTOM,expand=True)
-label.pack(side=BOTTOM,expand=True)
-heading = Label(top, text="Image Classification CIFAR10",pady=20, font=('arial',20,'bold'))
+upload.pack(side = BOTTOM, pady = 50)
+sign_image.pack(side = BOTTOM, expand = True)
+label.pack(side = BOTTOM, expand = True)
+heading = Label(top, text = "Image Classification CIFAR10",
+                pady = 20, font = ('arial', 20, 'bold'))
 
-heading.configure(background='#CDCDCD',foreground='#364156')
+heading.configure(background = '#CDCDCD', foreground = '#364156')
 heading.pack()
 top.mainloop()
